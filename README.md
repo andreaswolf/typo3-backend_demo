ff36f191970869502f2c487326ee17ec1a6beb55
Andreas Wolf <dev@a-w.io>
2017-02-11 12:24:46 +0100

    [TASK] Add basic extension structure

---
+++ README.md
@@ -0,0 +1,7 @@
+# Showcase for backend extensions
+
+This extension aims at showcasing all possibilities for modifying the backend. It wants to provide a commented, comprehensive overview of all extension points.
+
+## Features
+
+  * A classic (non-Extbase) backend module
---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Backend demo',
+    'description' => 'Demonstrates all available ',
+    'category' => 'module',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author' => 'Andreas Wolf',
+    'author_email' => 'dev@a-w.io',
+    'author_company' => '',
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '7.6.0-8.5.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
