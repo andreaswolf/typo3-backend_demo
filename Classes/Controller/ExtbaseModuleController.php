af3f54d4b94df2f166817ceadd4cb86573af1dfc
Andreas Wolf <dev@a-w.io>
2017-04-29 16:20:08 +0200

    [TASK] Add link button to icon bar

--- Classes/Controller/ExtbaseModuleController.php
+++ Classes/Controller/ExtbaseModuleController.php
@@ -1,7 +1,10 @@
 <?php
 namespace AndreasWolf\BackendDemo\Controller;
 
+use TYPO3\CMS\Backend\Template\Components\ButtonBar;
 use TYPO3\CMS\Backend\View\BackendTemplateView;
+use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class ExtbaseModuleController extends ActionController
@@ -16,6 +19,18 @@ class ExtbaseModuleController extends ActionController
      */
     protected $defaultViewObjectName = BackendTemplateView::class;
 
+    /** @var BackendTemplateView */
+    protected $view;
+
+    /** @var IconFactory */
+    protected $iconFactory;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->iconFactory = GeneralUtility::makeInstance(IconFactory::class);
+    }
+
     public function demoAction()
     {
         //
@@ -23,7 +38,14 @@ class ExtbaseModuleController extends ActionController
 
     public function secondAction()
     {
-        //
+        $buttonBar = $this->view->getModuleTemplate()->getDocHeaderComponent()->getButtonBar();
+
+        $inputButton = $buttonBar->makeLinkButton();
+        $inputButton->setHref('#')->setTitle('Demo link button')
+            ->setIcon($this->iconFactory->getIcon('actions-document-save', Icon::SIZE_SMALL))
+            ->setOnClick('alert("Button was clicked");')
+            ->setShowLabelText(true);
+        $buttonBar->addButton($inputButton, ButtonBar::BUTTON_POSITION_RIGHT);
     }
 
 }
